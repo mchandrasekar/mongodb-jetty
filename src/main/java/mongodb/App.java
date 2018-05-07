177c89f9cc95b10cc98ff32d5d8221f62c9d8929
mchand <mchand@us.ibm.com>
2018-05-06 19:53:06 -0700

    Test eclipse push

--- src/main/java/mongodb/App.java
+++ src/main/java/mongodb/App.java
@@ -1,31 +1,46 @@
 package mongodb;
 
+import java.net.UnknownHostException;
+
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
+import org.glassfish.jersey.servlet.ServletContainer;
+
+import com.mongodb.DB;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
 
 public class App {
 	public static void main(String[] args) throws Exception {
-        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
-        context.setContextPath("/");
+		ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+		context.setContextPath("/");
 
-        Server jettyServer = new Server(8091);
-        jettyServer.setHandler(context);
+		Server jettyServer = new Server(8091);
+		jettyServer.setHandler(context);
 
-        ServletHolder jerseyServlet = context.addServlet(
-             org.glassfish.jersey.servlet.ServletContainer.class, "/rest/*");
-        jerseyServlet.setInitOrder(0);
+		ServletHolder testServlet = context.addServlet(ServletContainer.class,"/rest/*");
+		testServlet.setInitOrder(0);
+		testServlet.setInitParameter("jersey.config.server.provider.classnames", EntryPoint.class.getCanonicalName());
+		
+		
+		ServletHolder personServlet = context.addServlet(ServletContainer.class, "/api/v1/*");
+		personServlet.setInitOrder(0);
+		personServlet.setInitParameter("jersey.config.server.provider.packages", "mongodb.Resources");
+		
+		context.setAttribute("mongodbObj", initializeMongoDb());
 
-        // Tells the Jersey Servlet which REST service/class to load.
-        jerseyServlet.setInitParameter(
-           "jersey.config.server.provider.classnames",
-           EntryPoint.class.getCanonicalName());
+		try {
+			jettyServer.start();
+			jettyServer.join();
+		} finally {
+			jettyServer.destroy();
+		}
+	}
 
-        try {
-            jettyServer.start();
-            jettyServer.join();
-        } finally {
-            jettyServer.destroy();
-        }
-    }
+	public static DB initializeMongoDb() throws UnknownHostException {
+		MongoClient mongoClient = new MongoClient(new MongoClientURI("mongodb://localhost:27017"));
+		DB database = mongoClient.getDB("test-db");
+		return database;
+	}
 }
--- src/main/java/mongodb/EntryPoint.java
+++ src/main/java/mongodb/EntryPoint.java
@@ -1,17 +1,21 @@
 package mongodb;
 
+import javax.servlet.ServletContext;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 
 @Path("entry-point")
 public class EntryPoint {
+	@Context ServletContext context;
 
     @GET
     @Path("test")
-    @Produces(MediaType.TEXT_PLAIN)
+    @Produces(MediaType.APPLICATION_JSON)
     public String test() {
+    	System.out.println(context.getAttribute("test"));
         return "Test";
     }
 }
